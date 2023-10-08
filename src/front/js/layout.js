a26efd748117be799bf2e0a7029e15767ea745a3
Daniel Carrión Martínez <Cardigo@MacBook-Pro-de-Daniel.local>
2023-10-08 12:52:52 +0200

    lo paso todo a pages y componentes

---
+++ src/front/js/layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import { BackendURL } from "./component/backendURL";
+
+import { Page1 } from "./pages/page1.js";
+
+
+//create your first component
+const Layout = () => {
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
+
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+
+    return (
+        <div>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar />
+                    <Routes>
+                        <Route element={<Page1 />} path="/" />
+
+                    </Routes>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default injectContext(Layout);
